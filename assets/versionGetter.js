2ea05f89e4e37fb011ad55423a1be9a504ac50b3
Jake Stout <kyjstout@gmail.com>
2024-02-21 21:58:42 -0500

    Add version box close button

--- assets/versionGetter.js
+++ assets/versionGetter.js
@@ -4,3 +4,9 @@ node.textContent = versionNumber;
 
 const dateNode = document.querySelector("#versionDate")
 dateNode.textContent = "2024-02-21";
+
+const button = document.querySelector("#versionBox button")
+button.addEventListener("click", function () {
+    const box = document.querySelector("#versionBox")
+    document.removeChild(box)
+});
