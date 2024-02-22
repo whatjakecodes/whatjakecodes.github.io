aa01541fd02b63ca7486282e3afcab3e598d41d3
Jake Stout <kyjstout@gmail.com>
2024-02-21 21:48:03 -0500

    Version shows date in bottom right corner

--- assets/versionGetter.js
+++ assets/versionGetter.js
@@ -1,3 +1,6 @@
 const node = document.querySelector("#versionNumber")
 const versionNumber = "vSoon";
 node.textContent = versionNumber;
+
+const dateNode = document.querySelector("#versionDate")
+dateNode.textContent = "2024-02-21";
