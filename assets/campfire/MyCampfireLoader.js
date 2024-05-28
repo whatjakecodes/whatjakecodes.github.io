0ea2fc17adf8362e9f9b89e8d450c64ec0ea83be
Jake Stout <kyjstout@gmail.com>
2024-05-28 13:45:05 -0400

    Disable reformatting on mobile device

--- assets/campfire/MyCampfireLoader.js
+++ assets/campfire/MyCampfireLoader.js
@@ -1,18 +1,18 @@
 
-if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-    // Mobile device style: fill the whole browser client area with the game canvas:
-    var meta = document.createElement('meta');
-    meta.name = 'viewport';
-    meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
-    document.getElementsByTagName('head')[0].appendChild(meta);
-
-    var canvas = document.querySelector("#unity-canvas");
-    canvas.style.width = "100%";
-    canvas.style.height = "100%";
-    canvas.style.position = "fixed";
-
-    document.body.style.textAlign = "left";
-}
+// if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+//     // Mobile device style: fill the whole browser client area with the game canvas:
+//     var meta = document.createElement('meta');
+//     meta.name = 'viewport';
+//     meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+//     document.getElementsByTagName('head')[0].appendChild(meta);
+//
+//     var canvas = document.querySelector("#unity-canvas");
+//     canvas.style.width = "100%";
+//     canvas.style.height = "100%";
+//     canvas.style.position = "fixed";
+//
+//     document.body.style.textAlign = "left";
+// }
 
 createUnityInstance(document.querySelector("#unity-canvas"), {
     dataUrl: "../../assets/campfire/Build/campfire4.data.unityweb",
